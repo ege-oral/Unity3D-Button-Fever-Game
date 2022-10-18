65c8984c2404b7602f017970fc7d9035d513014f
Ege Oral <egeoral@gmail.com>
2022-10-18 14:12:08 +0300

    Cubes added.

--- Assets/Scripts/Grabber.cs
+++ Assets/Scripts/Grabber.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Grabber : MonoBehaviour
 {
-    private GameObject selectedObject;
+    private GameObject selectedBlock;
     Touch touch;
     Camera mainCamera;
     private bool isPickedUp = false;
@@ -16,10 +16,11 @@ public class Grabber : MonoBehaviour
 
     void Start()
     {
+        mainCamera = Camera.main;
+
         // Grid start size is 6x6.
         // TO DO change it to 6x6.
         grid = new GameObject[3,6];
-        mainCamera = Camera.main;
         CreateGrid();
 
         
@@ -29,44 +30,50 @@ public class Grabber : MonoBehaviour
     {
         if(Input.touchCount > 0)
         {
-            touch = Input.GetTouch(0);    
-            RaycastHit hit = CastRay(touch);
-            if(hit.collider != null && hit.collider.gameObject.tag == "Block")
-            {   
-                selectedObject = hit.collider.gameObject;
-            }
+            touch = Input.GetTouch(0);
+            // If we have a selected block we don't have to Raycast every time.
+            if(selectedBlock == null)
+            {
+                RaycastHit hit = CastRay(touch);
+                if(hit.collider != null && hit.collider.gameObject.tag == "Block")
+                {   
+                    selectedBlock = hit.collider.gameObject;
+                }
+            }    
 
-            if(selectedObject != null)
+            if(selectedBlock != null)
             {
                 Vector3 position = new Vector3(touch.position.x, 
                                                 touch.position.y, 
-                                                mainCamera.WorldToScreenPoint(selectedObject.transform.position).z);
+                                                mainCamera.WorldToScreenPoint(selectedBlock.transform.position).z);
                 Vector3 worldPos = mainCamera.ScreenToWorldPoint(position);
                 if(!isPickedUp)
                 {
-                    selectedObject.transform.position = new Vector3(worldPos.x, worldPos.y, worldPos.z - 1f);
+                    selectedBlock.transform.position = new Vector3(worldPos.x, worldPos.y, worldPos.z - 1f);
                     isPickedUp = true;
                 }
-                selectedObject.transform.position = new Vector3(worldPos.x, worldPos.y, selectedObject.transform.position.z);
+                selectedBlock.transform.position = new Vector3(worldPos.x, worldPos.y, selectedBlock.transform.position.z);
                 CalcuateDistanceToNearestSwitch();
             }
         }
         else
         {
-            if(selectedObject != null)
+            if(selectedBlock != null)
             {
                 if(isPickedUp)
                 {
-                    selectedObject.transform.position = new Vector3(selectedObject.transform.position.x, 
-                                                                selectedObject.transform.position.y, 
-                                                                selectedObject.transform.position.z + 1f);
+                    selectedBlock.transform.position = new Vector3(selectedBlock.transform.position.x, 
+                                                                selectedBlock.transform.position.y, 
+                                                                selectedBlock.transform.position.z + 1f);
                     isPickedUp = false;
                 }
-
-                selectedObject.transform.position = new Vector3(nearestSwitch.transform.position.x, 
-                                                                nearestSwitch.transform.position.y, 
-                                                                selectedObject.transform.position.z);
-                selectedObject = null;
+                if(nearestSwitch != null)
+                {
+                    selectedBlock.transform.position = new Vector3(nearestSwitch.transform.position.x, 
+                                                                    nearestSwitch.transform.position.y, 
+                                                                    selectedBlock.transform.position.z);
+                }
+                selectedBlock = null;
             }
         }
     }
@@ -95,22 +102,27 @@ public class Grabber : MonoBehaviour
     private void CalcuateDistanceToNearestSwitch()
     {
         float minDistanceToSwitch = 99f;
-        GameObject _nearestSwitch = null;
+        GameObject tmpNearestSwitch = null;
         for(int row = 0; row < grid.GetLength(0); row++)
         {
             for(int col = 0; col < grid.GetLength(1); col++)
             {
                 // Ignoring z axis.
-                float distance = Vector2.Distance(grid[row,col].transform.position, selectedObject.transform.position);
-                if(distance < minDistanceToSwitch)
+                float distance = Vector2.Distance(grid[row,col].transform.position, selectedBlock.transform.position);
+                // Prevent far distance placing.
+                if(distance < 0.5f)
                 {
-                    minDistanceToSwitch = distance;
-                    _nearestSwitch = grid[row,col];
+                    if(distance < minDistanceToSwitch)
+                    {
+                        minDistanceToSwitch = distance;
+                        tmpNearestSwitch = grid[row,col];
+                    }
                 }
+                else
+                    nearestSwitch = null;
             }
         }
-        print($"Swithc: {_nearestSwitch.name} Distance: {minDistanceToSwitch}");
-        nearestSwitch = _nearestSwitch;
+        nearestSwitch = tmpNearestSwitch;
     }
 
     private void CreateGrid()
@@ -122,7 +134,6 @@ public class Grabber : MonoBehaviour
             {
                 grid[row,col] = switches[currentIndex];
                 currentIndex += 1;
-                print(grid[row, col]);
             }
         }
     }
